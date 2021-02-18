fb5650028380a4140d03dca4e3dcbf09c5482830
Anna Kryva <anna120100@gmail.com>
2021-02-19 00:38:15 +0200

    feat: add material-ui

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,24 @@
 import React from "react";
 import "./assets/style.scss";
+import { ThemeProvider, createMuiTheme } from "@material-ui/core/styles";
+
+const theme = createMuiTheme({
+    palette: {
+        primary: {
+            main: "#45190E",
+        },
+        secondary: {
+            main: "#FF7306",
+        },
+    },
+});
 
 function App() {
-    return <div className="App"></div>;
+    return (
+        <div className="App">
+            <ThemeProvider theme={theme}>{}</ThemeProvider>
+        </div>
+    );
 }
 
 export default App;
