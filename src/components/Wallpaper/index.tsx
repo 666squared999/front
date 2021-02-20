79af477585beb0ce3d96fbcf8fc9171976dab07f
Anna Kryva <anna120100@gmail.com>
2021-02-20 21:04:59 +0200

    fix: wallpaper texts

--- src/components/Wallpaper/index.tsx
+++ src/components/Wallpaper/index.tsx
@@ -5,9 +5,9 @@ export const Wallpaper: FC = () => {
     return (
         <div className="Wallpaper">
             <div className="container">
-                <p className="firstLine">Have you lost your solumate?</p>
+                <p className="firstLine">Have you lost your soulmate?</p>
                 <p className="secondLine">
-                    Or have you found pet and want help it to get home?
+                    Or have you found a pet and want help it to get home?
                 </p>
             </div>
         </div>
