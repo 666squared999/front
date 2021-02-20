23b4849507363f50148ea38c6b92e6435540ba8c
Anna Kryva <anna120100@gmail.com>
2021-02-20 18:10:29 +0200

    feat: add pages templates

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,7 +1,5 @@
 import React, { FC } from "react";
-import { Link } from "react-router-dom";
-import { Logo } from "../Logo";
-import { LANDING_PATH, Router } from "../Router";
+import { Router } from "../Router";
 import "./style.scss";
 
 export const Header: FC = () => {
---
+++ src/pages/About/index.tsx
@@ -0,0 +1,6 @@
+import { FC } from "react";
+import "./style.scss";
+
+export const About: FC = () => {
+    return <div className="About" />;
+};
