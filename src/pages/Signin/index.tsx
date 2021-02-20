b78152ee460e33f6ef498cc37c1ee93864b17af9
Anna Kryva <anna120100@gmail.com>
2021-02-20 18:13:35 +0200

    fix: rename signin page (camelCase)

--- src/pages/Signin/index.tsx
+++ src/pages/Signin/index.tsx
@@ -1,6 +1,6 @@
 import { FC } from "react";
 import "./style.scss";
 
-export const Signin: FC = () => {
-    return <div className="Signin" />;
+export const SignIn: FC = () => {
+    return <div className="SignIn" />;
 };
