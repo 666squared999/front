237e99640d8fa140182fa8287331d6b973cc6295
Anna Kryva <anna120100@gmail.com>
2021-02-20 19:15:58 +0200

    feat: add wallpaper component

---
+++ src/components/Wallpaper/index.tsx
@@ -0,0 +1,15 @@
+import React, { FC } from "react";
+import "./style.scss";
+
+export const Wallpaper: FC = () => {
+    return (
+        <div className="Wallpaper">
+            <div className="container">
+                <p className="firstLine">Have you lost your solumate?</p>
+                <p className="secondLine">
+                    Or have you found pet and want help it to get home?
+                </p>
+            </div>
+        </div>
+    );
+};
--- src/pages/Landing/index.tsx
+++ src/pages/Landing/index.tsx
@@ -1,6 +1,11 @@
 import { FC } from "react";
+import { Wallpaper } from "../../components/Wallpaper";
 import "./style.scss";
 
 export const Landing: FC = () => {
-    return <div className="Landing" />;
+    return (
+        <div className="Landing">
+            <Wallpaper />
+        </div>
+    );
 };
