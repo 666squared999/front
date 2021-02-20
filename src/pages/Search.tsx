29638be69f0d3acd8d003c104963ecf1c8f3b351
VladosK0k0s <p.s.vlad2014@gmail.com>
2021-02-20 18:16:37 +0200

    search filters initial

---
+++ src/pages/Search.tsx
@@ -0,0 +1,10 @@
+import React from "react";
+import { SearchFilters } from "../components/SearchFilters";
+
+export const Search = () => {
+    return (
+        <div className="Search">
+            <SearchFilters />
+        </div>
+    );
+};
