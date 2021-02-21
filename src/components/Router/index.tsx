2ee455b7137bfc2d9b897a378820f82fc7e2f133
Anna Kryva <anna120100@gmail.com>
2021-02-21 05:38:48 +0200

    feat: add user info

--- src/components/Router/index.tsx
+++ src/components/Router/index.tsx
@@ -1,11 +1,12 @@
 import React, { FC } from "react";
-import { BrowserRouter, Route, Switch } from "react-router-dom";
+import { BrowserRouter, Redirect, Route, Switch } from "react-router-dom";
 import { About } from "../../pages/About";
 import { Home } from "../../pages/Home";
 import { Landing } from "../../pages/Landing";
 import { Login } from "../../pages/Login";
 import { Search } from "../../pages/Search";
 import { Signin } from "../../pages/SignIn";
+import { useAuthContext } from "../../store/AuthContext";
 import { Header } from "../Header";
 import { PrivateRoute } from "../PrivateRoute/PrivateRoute";
 import "./style.scss";
@@ -18,6 +19,8 @@ export const SIGNIN_PATH = "/signin";
 export const HOME_PAGE = "/home";
 
 export const Router: FC = () => {
+    const { accessToken } = useAuthContext();
+
     return (
         <BrowserRouter>
             <Header />
@@ -31,12 +34,37 @@ export const Router: FC = () => {
                 <Route path={ABOUT_PATH}>
                     <About />
                 </Route>
-                <Route path={LOGIN_PATH}>
-                    <Login />
-                </Route>
-                <Route path={SIGNIN_PATH}>
-                    <Signin />
-                </Route>
+                <Route
+                    path={LOGIN_PATH}
+                    render={({ location }) =>
+                        !accessToken ? (
+                            <Login />
+                        ) : (
+                            <Redirect
+                                to={{
+                                    pathname: HOME_PAGE,
+                                    state: { from: location },
+                                }}
+                            />
+                        )
+                    }
+                />
+                <Route
+                    path={SIGNIN_PATH}
+                    render={({ location }) =>
+                        !accessToken ? (
+                            <Signin />
+                        ) : (
+                            <Redirect
+                                to={{
+                                    pathname: HOME_PAGE,
+                                    state: { from: location },
+                                }}
+                            />
+                        )
+                    }
+                />
+
                 <PrivateRoute path={HOME_PAGE}>
                     <Home />
                 </PrivateRoute>
--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,6 +1,31 @@
-import { FC } from "react";
+import React, { FC, useEffect, useState } from "react";
+import { getUserInfo } from "../../api/requests";
+import { User } from "../../components/User";
+import { useAuthContext } from "../../store/AuthContext";
 import "./style.scss";
 
 export const Home: FC = () => {
-    return <div className="Home" />;
+    const { accessToken, logout } = useAuthContext();
+    const [user, setUser] = useState<User>({
+        id: 0,
+        username: "",
+        email: "",
+    });
+
+    useEffect(() => {
+        if (!accessToken) {
+            return;
+        }
+
+        (async () => {
+            const state = await getUserInfo(accessToken);
+            setUser(state);
+        })();
+    }, [accessToken]);
+
+    return (
+        <div className="Home">
+            <User logout={logout} {...user} />
+        </div>
+    );
 };
